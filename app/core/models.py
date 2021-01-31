526acf0a3017c0559b569cb46c0d711e8042dbde
Jacob <jacob.juhyung.lee@gmail.com>
2021-01-31 13:50:18 +0900

    Add: custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -16,8 +16,8 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
-        user = self.create_user(email, password)
-        user.is_staff = True
+        user              = self.create_user(email, password)
+        user.is_staff     = True
         user.is_superuser = True
         user.save(using=self._db)
 
